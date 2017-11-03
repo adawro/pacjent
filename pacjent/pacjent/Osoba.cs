5d1955fffab93771f387bed4fef9997eb5f6d812
adawro <32652461+adawro@users.noreply.github.com>
2017-11-04 00:06:20 +0100

    Add files via upload

---
+++ pacjent/pacjent/Osoba.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pacjent
+{
+    class Osoba
+    {
+        private string imie;
+        private string nazwisko;
+        public Osoba(string imie, string nazwisko)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+        }
+        public void WypiszInfo()
+        {
+            Console.Write("{0} {1} ", imie,nazwisko);
+        }
+
+
+    }
+    class Pacjent : Osoba
+    {
+        private int wiek;
+
+        public Pacjent(string imie, string nazwisko, int wiek)
+            :base (imie,nazwisko)
+        {
+            this.wiek = wiek;
+
+        }
+        public void WypiszInfo()
+        {
+            base.WypiszInfo();
+            Console.WriteLine("{0}", wiek);
+        }
+
+    }
+}
+ 
---
+++ pacjent/pacjent/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pacjent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pacjent pacjent1 = new Pacjent("Adam", "po", 19);
+            pacjent1.WypiszInfo();
+            Console.ReadKey();
+
+        }
+    }
+}
